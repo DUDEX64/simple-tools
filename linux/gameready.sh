aded18600c7fa2e09ccd301d16c56a9583208177
DUDEX64 <DUDEX64@users.noreply.github.com>
2017-08-03 00:05:09 +1000

    Updated linux/gameready.sh

--- linux/gameready.sh
+++ linux/gameready.sh
@@ -13,6 +13,7 @@ if [ "$1" == "summer" ]; then
 	# This is the summer/laptop config
 	printf "Selecting Summer Mode...\n"
 	nvidia-smi -acp RESTRICTED &> /dev/null
+	nvidia-smi -pm DISABLED &> /dev/null
 	cpupower frequency-set -g powersave --min 775MHz --max 2GHz &> /dev/null
 	cpupower set -b 15
 	cpupower frequency-info
@@ -22,8 +23,19 @@ if [ "$1" == "winter" ]; then
 	# This is the winter/desktop config
 	printf "Selecting Winter Mode...\n"
 	nvidia-smi -acp UNRESTRICTED &> /dev/null
+	nvidia-smi -pm ENABLED &> /dev/null
 	cpupower frequency-set -g performance --min 775MHz --max 2GHz &> /dev/null
 	cpupower set -b 0
+	cpupower frequency-info
+	exit 0
+fi
+if [ "$1" == "normal" ]; then
+	# This is the normal config
+	printf "Selecting Normal Mode...\n"
+	nvidia-smi -acp UNRESTRICTED &> /dev/null
+	nvidia-smi -pm DISABLED &> /dev/null
+	cpupower frequency-set -g ondemand --min 775MHz --max 2GHz &> /dev/null
+	cpupower set -b 8
 	cpupower frequency-info
 	exit 0
 fi
